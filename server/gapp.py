6c93624ee4cef9a27a310b43a78db569c3e9feda
DexyScraft <[valid-email]>
2024-03-14 21:35:50 +0530

    update readme.md and fixed the env

--- server/gapp.py
+++ server/gapp.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 # Load environment variables
-load_dotenv()
+load_dotenv('.env.local')
 
 # Configure the API key for google.generativeai
 genai.configure(api_key=os.getenv('API_KEY'))
